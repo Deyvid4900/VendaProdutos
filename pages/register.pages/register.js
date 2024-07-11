b67e4fc54e4c6987fdbdbe8c816fdcd4f5e30e91
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2024-07-11 15:55:17 -0300

    :sparkles: ajuste da pag register

--- pages/register.pages/register.js
+++ pages/register.pages/register.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('form');
 
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -8,6 +10,30 @@ form.addEventListener('submit', (e) => {
     formData.forEach((value, key) => {
         data[key] = value;
     });
-    console.log('Dados do formulário:', data);
+
+    async function criarConta(data) {
+        const URL = "http://localhost:3000/user-controller/Usuario";
+        try {
+            const response = await fetch(URL, {
+                method: "POST",
+                mode: 'cors',
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            const novoUsuario = await response.json();
+            return novoUsuario;
+
+
+            
+
+        } catch (error) {
+            console.error("Ocorreu um erro:", error);
+        }
     
+    }
+    criarConta(data)
 });
