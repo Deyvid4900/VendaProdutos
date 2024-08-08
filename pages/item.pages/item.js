d2854afb672a2231738e20a1bf642150ee668e15
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2024-08-07 23:09:09 -0300

    dei continuidade na integração

--- pages/item.pages/item.js
+++ pages/item.pages/item.js
@@ -1,31 +1,53 @@
-async function buscarCursoPorId(codigo){
-    const URL = "http://localhost:3000/curso?id="+ codigo;
-    try {
-      const response = await fetch(URL, {
-        method: "GET",
-        mode: 'cors',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-        }
-      });
-  
-      const curso = await response.json();
-      return curso;
-  
-    } catch (error) {
-      console.error("Ocorreu um erro:", error);
-    }
+async function buscarCursoPorId(codigo) {
+  const URL = "http://localhost:3000/curso?id=" + codigo;
+  try {
+    const response = await fetch(URL, {
+      method: "GET",
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    const curso = await response.json();
+    return curso;
+
+  } catch (error) {
+    console.error("Ocorreu um erro:", error);
   }
-  
+}
 
-var queryString = window.location.search;
+async function exibirCurso() {
+  var queryString = window.location.search;
+  queryString = queryString.substring(1);
+  var searchParams = new URLSearchParams(queryString);
+  var id = searchParams.get('id');
 
-queryString = queryString.substring(1);
+  if (id) {
+    let curso = await buscarCursoPorId(id);
+    const categoria = document.getElementById('categoria');
+    const titulo = document.getElementById('titulo');
+    const descricao = document.getElementById('descricao');
+    const criador = document.getElementById('criador');
+    const img = document.getElementById('img');
+    const preco = document.getElementById('preco');
+    if (curso) {
 
-var searchParams = new URLSearchParams(queryString);
+      
+      if (categoria) categoria.innerHTML = curso.categoriaCurso || 'Não disponível';
+      if (titulo) titulo.innerHTML = curso.nomeCurso || 'Não disponível';
+      if (descricao) descricao.innerHTML = curso.descricaoCurso || 'Não disponível';
+      if (criador) criador.innerHTML = curso.criadorCurso || 'Não disponível';
+      if (img) img.setAttribute('src', curso.imgCurso)   || 'Não disponível';
+      if (preco) preco.innerHTML = "R$"+curso.valorCurso+',00'   || 'Não disponível';
 
-var id = searchParams.get('id');
-let item = buscarCursoPorId(id)
+    } else {
+      console.log("Curso não encontrado.");
+    }
+  } else {
+    console.log("ID do curso não fornecido na URL.");
+  }
+}
 
-console.log(item.nomeCurso)
+// Chamar a função assíncrona para exibir o curso
+exibirCurso();
